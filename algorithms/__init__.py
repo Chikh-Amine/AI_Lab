f8fa0d9728f8e20f5fc08a6fd09455668a67af2e
Chikh-Amine <chikhmedamine433@gmail.com>
2025-12-02 18:51:05 +0100

    added hill climbing

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -7,10 +7,12 @@ This package contains various algorithms for solving the Traveling Salesman Prob
 from .utils import calculate_distance, calculate_total_distance
 from .random_search import random_search
 from .local_search import local_search
+from .hill_climbing import hill_climbing
 
 __all__ = [
     'calculate_distance',
     'calculate_total_distance',
     'random_search',
     'local_search',
+    'hill_climbing',
 ]
